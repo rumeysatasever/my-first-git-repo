e6fd1699a4c824634a899717518eca84c051655b
rumeysatasever <rumeyskal@gmail.com>
2022-07-16 21:47:02 +0200

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -1,5 +1,7 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("I am learning GitHub!!");
+        System.out.println("I am adding new code...");
+        System.out.println("I am adding one more");
     }
 }
